8a2661bb1173eb8e3c065bfccd962478988fc7b0
Rajesh Pillai <rajesh.algorisys@ext.idfy.com>
2022-05-10 13:51:24 +0530

    broadcasts_to: shorthand method

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -13,13 +13,16 @@ class Quote < ApplicationRecord
   # after_destroy_commit -> { broadcast_remove_to "quotes" }
 
   # Use background job
-  after_create_commit -> { broadcast_prepend_later_to "quotes" }
-  after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
   
   
   #Note: The broadcast_remove_later_to method does not exist because as the quote gets 
   # deleted from the database, it would be impossible for a background job to retrieve 
   # this quote in the database later to perform the job.
   
-  after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+   # Those three callbacks are equivalent to the following single line
+   broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
