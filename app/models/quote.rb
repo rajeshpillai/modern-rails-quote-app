007984ed614770d9f0681d8b5013c56d20e91f0e
Rajesh Pillai <rajesh.algorisys@ext.idfy.com>
2022-05-10 13:49:29 +0530

    Use delayed job to update quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -8,7 +8,18 @@ class Quote < ApplicationRecord
   # }
 
   # Using default
-  after_create_commit -> { broadcast_prepend_to "quotes" }
-  after_update_commit -> { broadcast_replace_to "quotes" }
+  # after_create_commit -> { broadcast_prepend_to "quotes" }
+  # after_update_commit -> { broadcast_replace_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+  # Use background job
+  after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  after_update_commit -> { broadcast_replace_later_to "quotes" }
+  
+  
+  #Note: The broadcast_remove_later_to method does not exist because as the quote gets 
+  # deleted from the database, it would be impossible for a background job to retrieve 
+  # this quote in the database later to perform the job.
+  
   after_destroy_commit -> { broadcast_remove_to "quotes" }
 end
